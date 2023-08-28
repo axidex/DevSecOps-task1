4e0e8b5f6c2f1ccb5a1507cf6ea841c47dda9472
axidex <Stepworm@yandex.ru>
2023-08-28 14:38:48 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,8 +2,8 @@ mkdir app
 cd app
 
 rm -rf *
-git clone https://github.com/netlify/gocommerce
-mv gocommerce src
+git clone https://github.com/0c34/govwa # https://github.com/0c34/govwa https://github.com/netlify/gocommerce
+mv govwa src
 
 cd src && go build
 cd ..
@@ -11,12 +11,19 @@ wget https://github.com/CycloneDX/cyclonedx-gomod/releases/download/v1.4.1/cyclo
 tar -xvzf cyclonedx-gomod_1.4.1_linux_arm64.tar.gz
 cyclonedx-gomod app -output ./sbom.xml src
 
-curl -v -X "PUT" \
+curl -v -X "POST" \
     "http://localhost:8081/api/v1/bom" \
-    -d @out.json
-    -H 'X-API-Key: TimbOxMatBj7kSlCEq9KYJUoY70AsWmK' \
-    -H 'Content-Type: application/json'
+    -H "X-API-Key: TimbOxMatBj7kSlCEq9KYJUoY70AsWmK" \
+    -H "Content-Type: multipart/form-data" \
+    -F "autoCreate=true" \
+    -F "project=e561948b-93e1-4f86-8c04-1f10560df992" \
+    -F "projectName=tmp" \
+    -F "projectVersion=0.1" \
+    -F "bom=@./sbom.xml"
 
+# application/json
+#-H "Body: {project: e561948b-93e1-4f86-8c04-1f10560df992, bom: out.json}"
+sleep 10
 cp ../logger.py .
 python3.10 logger.py
 cat vuln.log
