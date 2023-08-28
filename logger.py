37750203f43beeacbb923141bc7e175e72dab721
axidex <Stepworm@yandex.ru>
2023-08-28 20:20:59 +0300

    Update logger.py

--- logger.py
+++ logger.py
@@ -9,24 +9,25 @@ def to_log(name, url, headers):
     with open(name, "w") as file:
         file.write(json.dumps(parsed, indent=2, sort_keys=True))
 
-def uuid_get(name, url, headers):
+def uuid_get(name, branch, url, headers):
     resp = requests.get(url,headers=headers)
-    print(resp.content)
-    print(type(resp.content))
     parsed = json.loads(resp.content)
     for el in parsed:
-        if el['name'] == name:
+        if el['name'] == name and el['version'] == branch:
             return el["uuid"]
-uuid = uuid_get( project_name, 'http://192.168.128.1:8081/api/v1/project', {"X-Api-Key": "TimbOxMatBj7kSlCEq9KYJUoY70AsWmK", "accept": "application/json"})
+        
 # Must have uuid http://localhost:8081/api/v1/metrics/project/{uuid}/current 
 # and api-key from dependency-tracker GUI below
 # to execute more cURL commands check htwagger.jtp://localhost:8081/api/sson with SwaggerUI plugin in ur browser
-
 headers = {"X-Api-Key": "TimbOxMatBj7kSlCEq9KYJUoY70AsWmK", "accept": "application/json"}
+uuid = uuid_get( project_name, project_branch,
+                 'http://192.168.128.1:8081/api/v1/project', 
+                 headers=headers )
 
-to_log(name =   'vuln.log', 
-       url =    'http://192.168.128.1:8081/api/v1/metrics/project/' + str(uuid) + '/current',
-       headers = headers)
-to_log(name =   'vuln_description.log',
-       url =    'http://192.168.128.1:8081/api/v1/vulnerability/project/' + str(uuid),
-       headers = headers)
+to_log( name =   'vuln.log', 
+        url =    'http://192.168.128.1:8081/api/v1/metrics/project/' + str(uuid) + '/current',
+        headers = headers )
+
+to_log( name =   'vuln_description.log',
+        url =    'http://192.168.128.1:8081/api/v1/vulnerability/project/' + str(uuid),
+        headers = headers )
